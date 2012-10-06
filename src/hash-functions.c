25a788047da82392efe49dd17efee66af11d7fc2
Joe Marrero <manvscode@gmail.com>
2012-10-06 17:36:15 -0400

    Added hash functions.

--- src/hash-functions.c
+++ src/hash-functions.c
@@ -28,10 +28,38 @@
 /*
  *   Hash Functions
  */
+static inline size_t int64_hash( int64_t key )
+{
+	key = (~key) + (key << 21); // key = (key << 21) - key - 1;
+	key = key ^ (key >> 24);
+	key = (key + (key << 3)) + (key << 8); // key * 265
+	key = key ^ (key >> 14);
+	key = (key + (key << 2)) + (key << 4); // key * 21
+	key = key ^ (key >> 28);
+	key = key + (key << 31);
+	return key;	
+}
+
+static inline size_t int32_hash( uint32_t a )
+{
+	a = (a+0x7ed55d16) + (a<<12);
+	a = (a^0xc761c23c) ^ (a>>19);
+	a = (a+0x165667b1) + (a<<5);
+	a = (a+0xd3a2646c) ^ (a<<9);
+	a = (a+0xfd7046c5) + (a<<3);
+	a = (a^0xb55a4f09) ^ (a>>16);
+	return a;
+}
+
 size_t pointer_hash( const void *data )
 {
+#if 0
 	return (size_t) data;
+#else
+	return int64_hash( (int64_t) data );
+#endif
 }
+
 
 size_t string_hash( const void *string_addr )
 {
